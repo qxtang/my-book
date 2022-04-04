7a2186da418a32da3a6b847e5272c3c7095fd5c1
qxtang <qx.tang@foxmail.com>
2022-04-04 09:19:36 +0000

    Deploying to gh-pages from  @ bbef8fe5c51cd1da2e29f30cd08c6efb62ab89c2 🚀

--- resource/script.js
+++ resource/script.js
@@ -1,14 +1,30 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function () {
-  // menu
+  var utils = {
+    getQueryString: function getQueryString(variable) {
+      var query = window.location.search.substring(1);
+      var vars = query.split('&');
+
+      for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+
+        if (pair[0] == variable) {
+          return decodeURIComponent(pair[1]);
+        }
+      }
+
+      return false;
+    }
+  }; // menu
+
   (function () {
-    var dirs = $('.dir');
+    var dirs = $('#menu .dir');
     dirs.on('click', function () {
       var p = $(this).parent('.parent');
       p.toggleClass('open');
     });
-    var links = $('.children > a');
+    var links = $('#menu .children > a');
     var host = window.location.protocol + '//' + window.location.host;
     var path = decodeURIComponent(window.location.pathname);
     links.each(function () {
@@ -22,14 +38,44 @@ document.addEventListener('DOMContentLoaded', function () {
         $(this).parent('.children').addClass('active');
       }
     });
-  })(); // viewer
+  })(); // menu drag
 
 
   (function () {
-    $('.markdown-body img').viewer({
-      title: false,
-      toolbar: false,
-      navbar: false
+    var drager = $('#drager');
+    var body = $('body');
+    var menu = $('#menu');
+    drager.mouseover(function () {
+      $(this).css('cursor', 'e-resize');
+    });
+    drager.mousedown(function () {
+      $(this).css('cursor', 'e-resize');
+      body.mousemove(function (e) {
+        var _x = e.pageX;
+
+        if (_x < 245) {
+          return;
+        }
+
+        menu.animate({
+          width: _x
+        }, 0);
+      });
+    });
+    body.mouseup(function () {
+      $(this).unbind('mousemove');
+      $(this).css('cursor', 'default');
+    });
+  })(); // menu switcher
+
+
+  (function () {
+    var menu = $('#menu');
+    var switcher = $('#drager > #switcher');
+    switcher.on('click', function () {
+      menu.removeAttr('style');
+      menu.toggleClass('expand');
+      $(this).toggleClass('expand');
     });
   })(); // mobile_menu
 
@@ -39,6 +85,135 @@ document.addEventListener('DOMContentLoaded', function () {
     var menu = $('#menu');
     mobile_menu.on('click', function () {
       menu.toggleClass('show');
+    });
+  })(); // toc active
+
+
+  (function () {
+    var fn = function fn() {
+      var links = $('.table-of-contents a');
+      links.each(function () {
+        var anchor = $(this).data('anchor');
+        var hash = window.location.hash;
+
+        if (anchor === hash) {
+          $(this).addClass('active');
+        } else {
+          $(this).removeClass('active');
+        }
+      });
+    };
+
+    fn();
+
+    if ('onhashchange' in window) {
+      window.onhashchange = fn;
+    }
+  })(); // search_bar
+
+
+  (function () {
+    var input = $('#search_bar > input');
+    var clear = $('#search_bar > #clear');
+    var timer = null;
+
+    var getSearchResult = function getSearchResult(str) {
+      str = str.toLowerCase();
+      var tree = window.__doc_builder_dirTree__ || [];
+      var res = [];
+
+      var fn = function fn(arr) {
+        arr.forEach(function (info) {
+          var isDir = !!info.dirname;
+
+          if (isDir) {
+            fn(info.children);
+          } else {
+            var findInTitle = info.basename.toLowerCase().indexOf(str) !== -1;
+            var findInContent = info.content.toLowerCase().indexOf(str) !== -1;
+
+            if (findInTitle || findInContent) {
+              res.push(info);
+            }
+          }
+        });
+      };
+
+      fn(tree);
+      return res;
+    };
+
+    var addHighlight = function addHighlight(str, keyword) {
+      var regExp = new RegExp(keyword, 'gi');
+      var text = regExp.exec(str);
+      return str.replace(regExp, '<mark class="keyword">' + ((text === null || text === void 0 ? void 0 : text[0]) || keyword) + '</mark>');
+    };
+
+    var handleInputChange = function handleInputChange(value) {
+      value = value.trim().toLowerCase();
+      var res = getSearchResult(value);
+      var showSearchResult = value.length !== 0;
+      var showEmpty = res.length === 0;
+      var wrapEle = $('#search_result');
+      var html = '';
+
+      if (value.length !== 0) {
+        clear.show();
+      } else {
+        clear.hide();
+      }
+
+      if (showEmpty) {
+        html = '<div class="empty">No Results!</div>';
+      } else {
+        html = res.map(function (info) {
+          var index = info.content.toLowerCase().indexOf(value);
+          var summary = "...".concat(info.content.substring(index, index + 30), "...");
+          var href = "".concat(window.root, "/").concat(info.relative_path ? info.relative_path + '/' : '').concat(info.basename, ".html?search=").concat(value);
+          return "\n              <a class=\"item\" href=\"".concat(href, "\">\n                <div class=\"title\">").concat(addHighlight(info.basename, value), "</div>\n                <div class=\"content\">").concat(addHighlight(summary, value), "</div>\n              </a>\n            ");
+        }).join('');
+      }
+
+      if (showSearchResult) {
+        wrapEle.html(html);
+        wrapEle.show();
+      } else {
+        wrapEle.hide();
+      }
+    };
+
+    input.bind('input propertychange', function (e) {
+      if (timer) {
+        clearTimeout(timer);
+      }
+
+      timer = setTimeout(function () {
+        handleInputChange(e.target.value);
+      }, 500);
+    });
+    clear.on('click', function () {
+      input.val('');
+      handleInputChange('');
+    });
+  })(); // keyword highlight
+
+
+  (function () {
+    var search = utils.getQueryString('search');
+
+    if (search) {
+      $('.content.markdown-body').mark(search, {
+        className: 'keyword'
+      });
+    }
+  })(); // viewer
+
+
+  (function () {
+    $('.markdown-body img').viewer({
+      title: false,
+      toolbar: false,
+      navbar: false
     });
   })();
 });
